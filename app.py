3953a41d2ed582e3456de83b99b7e77357c59b62
wolflergf <wolflergf@gmail.com>
2023-06-19 17:51:32 +0100

    meke comments on app file

--- app.py
+++ app.py
@@ -3,14 +3,17 @@ from flask import Flask, render_template, request
 from dotenv import dotenv_values
 import json
 
+# Load environment variables from .env file
 config = dotenv_values('.env')
+# Set OpenAI API key
 openai.api_key = config["OPENAI_API_KEY"]
 
-
+# Create Flask application instance
 app = Flask(__name__)
 
 
 def get_colors(msg):
+    # Define a prompt for generating color palettes based on text prompts
     prompt = f"""
     You are a color palette generating assistant that responds to text prompts for color palettes
     Your should generate color palettes that fit the theme, mood, or instructions in the prompt.
@@ -28,13 +31,14 @@ def get_colors(msg):
     Q: Convert the following verbal description of a color palette into a list of colors: {msg} 
     A:
     """
-
+    # Send completion request to OpenAI API to generate color palette
     response = openai.Completion.create(
         prompt=prompt,
         model="text-davinci-003",
         max_tokens=200,
     )
 
+    # Extract the generated colors from the API response
     colors = json.loads(response["choices"][0]["text"])
     return colors
 
@@ -46,6 +50,7 @@ def prompt_to_palette():
     return {"colors": colors}
 
 
+# Route to handle POST requests for generating color palettes
 @app.route("/")
 def index():
     return render_template("index.html")
